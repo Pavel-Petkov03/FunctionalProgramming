3444f118e13e786a7d067eae814c72885f09facf
Pavel Petkov <elias5247698@abv.bg>
2024-10-09 12:18:05 +0300

    pushing something from yesterday I dont remember

--- Scheme/Week02/exercise.scm
+++ Scheme/Week02/exercise.scm
@@ -52,3 +52,59 @@
   (umen-prime-inner num 2)
   )
 
+
+
+
+
+
+
+
+
+; seminar
+
+(define (custom-fact n)
+  (if (< n 0)
+      "Cannot find fact with negative number"
+      (if (= n 0)
+      1
+      (* n (custom-fact (- n 1)))
+       )
+      )
+  )
+ 
+(define add +)
+
+
+(define (count-digits n)
+  (define (count-digits-recurse n iter)
+    (if (= n 0)
+        iter
+        (count-digits-recurse (quotient n 10) (+ iter 1)) ; tail rec
+        )
+    )
+  (if (= n 0)
+      1
+      (count-digits n 0)
+      )
+  )
+
+
+(define (list-sum arr)
+  (define (list-sum-inner arr iter)
+    (if (null? arr)
+        iter
+        (list-sum-inner (cdr arr) (+ iter (car arr))) ; cdr -> takes everything except for the first element
+                                                      ; car -> takes the first element
+        )
+    )
+  (list-sum-inner arr 0)
+  )
+
+
+
+
+
+
+
+
+
