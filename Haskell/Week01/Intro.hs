4d90171410c2a9ce37cb0c288938c7e3f5dba876
Pavel Petkov <elias5247698@abv.bg>
2024-10-09 13:39:30 +0300

    adding some haskell

---
+++ Haskell/Week01/Intro.hs
@@ -0,0 +1,27 @@
+-- {{{ Pragmas
+
+-- NOTE: read up more here - https://wiki.haskell.org/Language_Pragmas
+
+-- cover all cases!
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+-- warn about incomplete patterns v2
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-}
+-- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+-- use different names!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}
+-- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}
+
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+-- disable some hints that spoil the easy tasks
+{-# HLINT ignore "Use even" #-}
+
+-- }}}
+
+
+fact :: Integer -> Integer
+fact 0 = 1
+fact n = n * fact (n - 1)
+
+-- >>> fact 5
