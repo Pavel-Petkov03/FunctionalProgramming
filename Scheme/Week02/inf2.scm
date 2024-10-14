9192b3af532254c8e64b6bcbf690c11d7b032ab8
Pavel Petkov <elias5247698@abv.bg>
2024-10-14 09:57:53 +0300

    sorting number[very hard without array]

--- Scheme/Week02/inf2.scm
+++ Scheme/Week02/inf2.scm
@@ -1,21 +1,74 @@
 #lang scheme
 
-(define (partial-fraction n)
-  
-  )
-
-
-
-(define (partial-fraction-wrapper current gen accumulator n)
-  (cond
-    [(= n (+ current gen)) (+ accumulator 1)]
-    [(> (+ current gen) n) accumulator]
-    [
-     ]
+; ще решавам само интересните задачи
+(define (partition-function-wrapper current end gen accumulator)
+  (define (partition-function-wrapper-looper i accumulator)
+    ( if (= i end)
+         accumulator
+         ( partition-function-wrapper-looper (+ i 1)
+                                           {cond
+                                            [(= (+ i gen) end) (+ accumulator 1)]
+                                            [ (> (+ i gen) end) accumulator]
+                                            [else
+                                                (partition-function-wrapper i end (+ i gen) accumulator)
+                                             ]
+                                            }
+                                           )
+           
     )
   )
+  (partition-function-wrapper-looper current accumulator))
 
 
 
+(define (partition-function n )
+  (partition-function-wrapper 1 n 0 1)
+  )
+
+
+
+
+(define (get-num-len-wrapper number)
+  (if (= number 0)
+      0
+      (+ 1 (get-num-len-wrapper (quotient number 10))
+      )
+  )
+  )
+
+(define (get-number-len number)
+  (if (= number 0)
+      1
+      (get-num-len-wrapper number)
+      )
+  )
+
+(define (get-number-index-wrapper whole-number searched iter-index)
+  (if (or (= (remainder whole-number 10) searched) (= whole-number 0))
+      iter-index
+      (get-number-index-wrapper (quotient whole-number 10) searched (+ 1 iter-index))
+      )
+  )
+
+(define (get-number-index whole-number searched)
+  (get-number-index-wrapper whole-number searched 0)
+  )
+
+
+(define (is-number-found? whole-number searched)
+  (not (= (get-number-index whole-number searched) (get-number-len whole-number)))
+  )
+
+
+(define (sort-n-iter n iter result)
+  (if (= iter 0)
+      (sort-n-iter (remove-zeroes n) (+ iter 1) )
+      )
+  )
+(define (sort-n n) ; ясно е че counting-sort e the best ама не мога да го използвам
+                   ;защото нямам право на масиви а няма да вкарам 10 променливи
+                   ; затова просто ще взимам индекса на най - голямото число ще го изваждам докато не стане n 0 и ще пълня друго число
+  #t
+  )
 
 
