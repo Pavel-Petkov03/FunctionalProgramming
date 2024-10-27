4ff95bb6aa08b58a99d053e82565bdfa7e8bf68b
Pavel Petkov <elias5247698@abv.bg>
2024-10-27 21:01:09 +0200

    finalising inf exercises

--- Scheme/Week05/inf.scm
+++ Scheme/Week05/inf.scm
@@ -96,6 +96,83 @@
   (transpose (zero-rows (transpose xss))) ; dosta maloumno ama e krasivo ngl
   )
 
+(define (main-diagonal xss)
+  (define (iter i xss)
+    (if (null? xss) '()
+      (cons (list-ref (car xss) i) (iter (+ i 1) (cdr xss)) )
+      )
+    )
+  (iter 0 xss)
+  )
+(define (secondary-diagonal xss)
+  (define (iter i xss)
+    (if (null? xss) '()
+      (cons (list-ref (car xss) i) (iter (- i 1) (cdr xss)) )
+      )
+    )
+  (iter (- (length xss) 1) xss)
+  )
+
+(define (diagonals xss)
+  (cons (main-diagonal xss) (list (secondary-diagonal xss)))
+  )
+
+
+
+(define (triangular? xss)
+  (define (iter xss i)
+    (cond
+      [(null? xss) #t]
+      [(all? (lambda (y) (= y 0)) (take (car xss) i)) (iter (cdr xss) (+ i 1))]
+      [else #f]
+      )
+  )
+  (iter xss 0)
+)
+; след доста опити осъзнах че трябва да го направя итеративно по скоро
+; този spiral matrix че нещо не ми идва рекурсията
+
+
+(define (pos row col xss)
+  (list-ref (list-ref xss row) col)
+  )
+
+(define (reverse-range b a)
+  (if (<= b a) '()
+      (cons b (reverse-range (- b 1) a))
+      )
+  )
+
+(define (spiral xss)
+  (define (additional-wrapper desired-length)
+     (define (iter up down left right res)
+       (if (= (length res) desired-length)
+           res
+           (let
+               (
+                [left-to-right (map (lambda (col-index) (pos up col-index xss)) (range left (+ 1 right)))]
+                [up-to-down (map (lambda (row-index) (pos row-index right xss)) (range (+ 1 up) (+ 1 down)))]
+                [right-to-left (map (lambda (current-col) (pos down current-col xss)) (reverse-range (- right 1) (- left 1)))]
+                [down-to-up (map (lambda (current-row) (pos current-row left xss)) (reverse-range (- down 1) up))]
+                )
+             (iter (+ 1 up) (- down 1) (+ 1 left) (- right 1) (append res
+                                                                       left-to-right
+                                                                       up-to-down
+                                                                       right-to-left
+                                                                       down-to-up
+                                                                       )
+                   )
+         
+             )
+       )
+    )
+    (iter 0 ( - (length xss) 1) 0 (- (length (car xss)) 1) '())
+  ;на всяка итерация се маха един layer на квадрата
+  )
+  (additional-wrapper (* (length (car xss)) (length xss)))
+  )
+  ;tazi zadacha mi vze zdraveto
+
 
 
 
